780b373ec7b376a7560a3ad5af20f4a79ee817c9
André Ferreira <andre.ferreira.alves@gmail.com>
2021-08-30 21:12:12 -0300

    melhorias no layout

--- App.js
+++ App.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import Home from './src/';
+import React from "react";
+import { StyleSheet, SafeAreaView } from "react-native";
+import ListaProdutos from "./src/views/ListaProdutos/ListaProdutos";
 
 export default function App() {
   return (
-    <Home />
+    <SafeAreaView style={styles.container}>
+      <ListaProdutos />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F4F0F4",
+  },
+});
