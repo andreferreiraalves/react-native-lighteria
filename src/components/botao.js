66af22ec758e35604f9707c2c4c0ece63d7a8559
André Ferreira <andre.ferreira.alves@gmail.com>
2021-09-01 19:36:18 -0300

    detalhes do produto

---
+++ src/components/botao.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  FONT_FAMILY_SEMI_BOLD,
+  FONT_SIZE_MEDIUM,
+  LIGHTBLUE,
+  WHITE,
+} from "../styles/styles";
+
+export const Botao = ({ titulo, onPress }) => {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View style={styles.container}>
+        <Text style={styles.botaoTexto}>{titulo}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: LIGHTBLUE,
+    padding: 20,
+    borderRadius: 6,
+  },
+  botaoTexto: {
+    //fontFamily: FONT_FAMILY_SEMI_BOLD
+    fontSize: FONT_SIZE_MEDIUM,
+    color: WHITE,
+  },
+});
